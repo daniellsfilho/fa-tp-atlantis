7ae56268d78cf4c908ab36c019caf2e247e64efe
Daniel <daniel.2308@outlook.com>
2022-06-23 16:40:02 -0300

    Adicao dos codigos da atvi atlantis

---
+++ atvi-atlantis/teste/index.ts
@@ -0,0 +1,47 @@
+import Cliente from "../modelos/cliente";
+import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
+let cliente = new Cliente()
+cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
+cliente.nomeSocial = `Dom Pedro II`
+cliente.dataCadastro = new Date(1840, 6, 23)
+cliente.dataNascimento = new Date(1825, 11, 2)
+let endereco = new Endereco()
+endereco.rua = `R. do Catete`
+endereco.bairro = `Copacabana`
+endereco.cidade = `Rio de Janeiro`
+endereco.estado = `Rio de Janeiro`
+endereco.pais = `Brasil`
+endereco.codigoPostal = `22220-000`
+cliente.endereco = endereco
+
+let telefone = new Telefone()
+telefone.ddd = '12'
+telefone.numero = '4002-8922'
+
+//TESTE COM 2 TELEFONES
+//let telefone2 = new Telefone()
+//telefone2.ddd='11'
+//telefone2.numero = '1234-5678'
+
+cliente.telefones.push(telefone)
+//cliente.telefones.push(telefone2)
+
+let dependente = new Cliente()
+dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
+dependente.nomeSocial = `Princesa Isabel`
+dependente.dataCadastro = new Date(1921, 10, 14)
+dependente.dataNascimento = new Date(1846, 6, 29)
+dependente.endereco = (cliente.endereco.clonar() as Endereco)
+let telefoneDependente
+
+cliente.telefones.forEach(telefone => {
+    telefoneDependente = (telefone.clonar() as Telefone)
+    dependente.telefones.push(telefoneDependente)
+});
+
+dependente.titular = cliente
+cliente.dependentes.push(dependente)
+
+console.log(cliente);
+console.log(dependente);
